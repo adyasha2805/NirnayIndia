41c400c7f2b65069fa7ada1fb27ae9acb7a811ae
adyasha2805 <adyashasuvadarshini@gmail.com>
2025-07-03 12:42:13 +0530

    Update Candidate.js

--- Backend/models/Candidate.js
+++ Backend/models/Candidate.js
@@ -1,1 +1,12 @@
+// models/Candidate.js
+const mongoose = require("mongoose");
 
+const candidateSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  party: { type: String, required: true },
+  state: { type: String, required: true },
+  logo: { type: String }, // logo URL
+  votes: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model("Candidate", candidateSchema);
