a274d5aeeb7e7abaa3c679d509b5ea462a1e53d5
adyasha2805 <adyashasuvadarshini@gmail.com>
2025-07-03 12:41:03 +0530

    Update admin.js

--- Backend/routes/admin.js
+++ Backend/routes/admin.js
@@ -1,1 +1,14 @@
+// routes/adminRoutes.js
+const express = require("express");
+const router = express.Router();
+const {
+  addCandidate,
+  deleteCandidate,
+  getResults
+} = require("../controllers/adminController");
 
+router.post("/candidates", addCandidate);
+router.delete("/candidates", deleteCandidate);
+router.get("/results", getResults);
+
+module.exports = router;
