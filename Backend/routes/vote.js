b23cb14de9c3b4bc876eba86330921477bb473c9
adyasha2805 <adyashasuvadarshini@gmail.com>
2025-07-03 12:41:42 +0530

    Update vote.js

--- Backend/routes/vote.js
+++ Backend/routes/vote.js
@@ -1,1 +1,9 @@
+// routes/voteRoutes.js
+const express = require("express");
+const router = express.Router();
+const { castVote } = require("../controllers/voteController");
+const authMiddleware = require("../middleware/authMiddleware");
 
+router.post("/", authMiddleware, castVote);
+
+module.exports = router;
