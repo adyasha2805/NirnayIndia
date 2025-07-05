82f9fe13670a301fdf14211335358c4d8f722045
adyasha2805 <adyashasuvadarshini@gmail.com>
2025-07-05 17:53:49 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,11 +1,10 @@
-// config/db.js
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000,
+      family: 4  // Force IPv4
     });
     console.log("✅ MongoDB Connected");
   } catch (error) {
